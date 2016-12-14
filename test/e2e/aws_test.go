e64e2d1adfb1f37451eedff23bb42f325465611f
Hongchao Deng <hongchaodeng1@gmail.com>
2016-12-14 13:16:45 -0800

    s3_test: add warning in t.Skip()

--- test/e2e/aws_test.go
+++ test/e2e/aws_test.go
@@ -9,14 +9,14 @@ import (
 
 func TestS3MajorityDown(t *testing.T) {
 	if os.Getenv("AWS_TEST_ENABLED") != "true" {
-		t.Skip()
+		t.Skip("skipping test since AWS_TEST_ENABLED is not set.")
 	}
 	testDisasterRecoveryWithStorageType(t, 2, spec.BackupStorageTypeS3)
 }
 
 func TestS3AllDown(t *testing.T) {
 	if os.Getenv("AWS_TEST_ENABLED") != "true" {
-		t.Skip()
+		t.Skip("skipping test since AWS_TEST_ENABLED is not set.")
 	}
 	testDisasterRecoveryWithStorageType(t, 3, spec.BackupStorageTypeS3)
 }
