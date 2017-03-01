3e84247b60bf586b2768ca17c55d8abf20220674
Hongchao Deng <hongchaodeng1@gmail.com>
2017-03-01 13:28:04 -0800

    ci: run e2e against k8s master

---
+++ hack/ci/get_kube.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+: ${DOWNLOAD_DIR:?"Need to set DOWNLOAD_DIR"}
+KUBE_RELEASE_URL=${KUBE_RELEASE_URL:-kubernetes-release-dev/ci}
+
+KUBE_VERSION=$(gsutil cat gs://${KUBE_RELEASE_URL}/latest.txt)
+gsutil cp "gs://${KUBE_RELEASE_URL}/${KUBE_VERSION}/kubernetes.tar.gz" "${DOWNLOAD_DIR}/"
+
+cd $DOWNLOAD_DIR
+tar zxvf kubernetes.tar.gz
+
+gsutil cp "gs://${KUBE_RELEASE_URL}/${KUBE_VERSION}/kubernetes-server-linux-amd64.tar.gz" "kubernetes/server/"
