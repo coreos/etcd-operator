8275422f9a1bee218fe3da19789613b84c51ca4f
Hongchao Deng <hongchaodeng1@gmail.com>
2017-03-03 10:44:51 -0800

    ci: mute untar message

--- hack/ci/get_kube.sh
+++ hack/ci/get_kube.sh
@@ -11,6 +11,6 @@ KUBE_VERSION=$(gsutil cat gs://${KUBE_RELEASE_URL}/latest.txt)
 gsutil cp "gs://${KUBE_RELEASE_URL}/${KUBE_VERSION}/kubernetes.tar.gz" "${DOWNLOAD_DIR}/"
 
 cd $DOWNLOAD_DIR
-tar zxvf kubernetes.tar.gz
+tar zxvf kubernetes.tar.gz 1>/dev/null
 
 gsutil cp "gs://${KUBE_RELEASE_URL}/${KUBE_VERSION}/kubernetes-server-linux-amd64.tar.gz" "kubernetes/server/"
