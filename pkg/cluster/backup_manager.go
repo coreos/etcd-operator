3061bfd0027971a95d47b1b9d572b891f59e2119
Hongchao Deng <hongchaodeng1@gmail.com>
2017-03-19 22:16:53 -0700

    *: pass nodeSelector from pod spec to backup sidecar

--- pkg/cluster/backup_manager.go
+++ pkg/cluster/backup_manager.go
@@ -103,7 +103,7 @@ func (bm *backupManager) setup() error {
 
 func (bm *backupManager) runSidecar() error {
 	cl, c := bm.cluster, bm.config
-	podSpec, err := k8sutil.NewBackupPodSpec(cl.Metadata.Name, bm.config.ServiceAccount, cl.Spec.Backup)
+	podSpec, err := k8sutil.NewBackupPodSpec(cl.Metadata.Name, bm.config.ServiceAccount, cl.Spec)
 	if err != nil {
 		return err
 	}
