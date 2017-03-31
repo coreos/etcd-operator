c5ce5e2e186885ffa6c8177583e0dcf597472598
Hongchao Deng <hongchaodeng1@gmail.com>
2017-03-31 15:04:10 +0800

    contorller: add test to cover events for failed cluster

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -148,24 +148,25 @@ func (c *Controller) Run() error {
 
 		for ev := range eventCh {
 			pt.start()
-			c.handleClusterEvent(ev)
+			if err := c.handleClusterEvent(ev); err != nil {
+				c.logger.Warningf("fail to handle event: %v", err)
+			}
 			pt.stop()
 		}
 	}()
 	return <-errCh
 }
 
-func (c *Controller) handleClusterEvent(event *Event) {
+func (c *Controller) handleClusterEvent(event *Event) error {
 	clus := event.Object
 
 	if clus.Status.IsFailed() {
 		if event.Type == kwatch.Deleted {
 			delete(c.clusters, clus.Metadata.Name)
 			delete(c.clusterRVs, clus.Metadata.Name)
-		} else {
-			c.logger.Infof("ignore failed cluster (%s). Please delete its TPR", clus.Metadata.Name)
+			return nil
 		}
-		return
+		return fmt.Errorf("ignore failed cluster (%s). Please delete its TPR", clus.Metadata.Name)
 	}
 
 	clus.Spec.Cleanup()
@@ -185,8 +186,7 @@ func (c *Controller) handleClusterEvent(event *Event) {
 
 	case kwatch.Modified:
 		if _, ok := c.clusters[clus.Metadata.Name]; !ok {
-			c.logger.Warningf("unsafe state. cluster was never created but we received event (%s)", event.Type)
-			return
+			return fmt.Errorf("unsafe state. cluster was never created but we received event (%s)", event.Type)
 		}
 		c.clusters[clus.Metadata.Name].Update(clus)
 		c.clusterRVs[clus.Metadata.Name] = clus.Metadata.ResourceVersion
@@ -194,8 +194,7 @@ func (c *Controller) handleClusterEvent(event *Event) {
 
 	case kwatch.Deleted:
 		if _, ok := c.clusters[clus.Metadata.Name]; !ok {
-			c.logger.Warningf("unsafe state. cluster was never created but we received event (%s)", event.Type)
-			return
+			return fmt.Errorf("unsafe state. cluster was never created but we received event (%s)", event.Type)
 		}
 		c.clusters[clus.Metadata.Name].Delete()
 		delete(c.clusters, clus.Metadata.Name)
@@ -204,6 +203,7 @@ func (c *Controller) handleClusterEvent(event *Event) {
 		clustersDeleted.Inc()
 		clustersTotal.Dec()
 	}
+	return nil
 }
 
 func (c *Controller) findAllClusters() (string, error) {
