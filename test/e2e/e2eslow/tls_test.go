38f0918baf076a489e78ed5129a8460729d5e544
Hongchao Deng <hongchaodeng1@gmail.com>
2017-06-29 14:53:01 -0700

    e2e: fix tls tests interference
    We shouldn't have two tests share anything to guard async deletion.

--- test/e2e/e2eslow/tls_test.go
+++ test/e2e/e2eslow/tls_test.go
@@ -15,9 +15,10 @@
 package e2eslow
 
 import (
+	"fmt"
 	"io/ioutil"
+	"math/rand"
 	"os"
-	"sync"
 	"testing"
 	"time"
 
@@ -26,33 +27,38 @@ import (
 	"github.com/coreos/etcd-operator/test/e2e/framework"
 )
 
-var createTLSSecretsOnce sync.Once
-
 func TestTLS(t *testing.T) {
 	testTLS(t, false)
 }
 
 func testTLS(t *testing.T, selfHosted bool) {
 	f := framework.Global
-	clusterName := "tls-test"
-	memberPeerTLSSecret := "etcd-peer-tls"
-	memberClientTLSSecret := "etcd-server-tls"
-	operatorClientTLSSecret := "etcd-client-tls"
-	createTLSSecretsOnce.Do(func() {
-		err := e2eutil.PreparePeerTLSSecret(clusterName, f.Namespace, memberPeerTLSSecret)
+	suffix := fmt.Sprintf("-%d", rand.Uint64())
+	clusterName := "tls-test" + suffix
+	memberPeerTLSSecret := "etcd-peer-tls" + suffix
+	memberClientTLSSecret := "etcd-server-tls" + suffix
+	operatorClientTLSSecret := "etcd-client-tls" + suffix
+
+	err := e2eutil.PreparePeerTLSSecret(clusterName, f.Namespace, memberPeerTLSSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+	certsDir, err := ioutil.TempDir("", "etcd-operator-tls-")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(certsDir)
+	err = e2eutil.PrepareClientTLSSecret(certsDir, clusterName, f.Namespace, memberClientTLSSecret, operatorClientTLSSecret)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer func() {
+		err := e2eutil.DeleteSecrets(f.KubeClient, f.Namespace, memberPeerTLSSecret, memberClientTLSSecret, operatorClientTLSSecret)
 		if err != nil {
 			t.Fatal(err)
 		}
-		certsDir, err := ioutil.TempDir("", "etcd-operator-tls-")
-		if err != nil {
-			t.Fatal(err)
-		}
-		defer os.RemoveAll(certsDir)
-		err = e2eutil.PrepareClientTLSSecret(certsDir, clusterName, f.Namespace, memberClientTLSSecret, operatorClientTLSSecret)
-		if err != nil {
-			t.Fatal(err)
-		}
-	})
+	}()
 
 	c := e2eutil.NewCluster("", 3)
 	c.Metadata.Name = clusterName
@@ -68,7 +74,7 @@ func testTLS(t *testing.T, selfHosted bool) {
 	if selfHosted {
 		c = e2eutil.ClusterWithSelfHosted(c, &spec.SelfHostedPolicy{})
 	}
-	c, err := e2eutil.CreateCluster(t, f.KubeClient, f.Namespace, c)
+	c, err = e2eutil.CreateCluster(t, f.KubeClient, f.Namespace, c)
 	if err != nil {
 		t.Fatal(err)
 	}
