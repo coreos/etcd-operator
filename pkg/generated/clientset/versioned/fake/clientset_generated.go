975943c73c607fb516d382e32bfd7cc11fefaf85
Hongchao Deng <hongchaodeng1@gmail.com>
2017-09-21 12:01:19 -0700

    generated: codegen clientset, informer, listers for etcd/v1beta2

---
+++ pkg/generated/clientset/versioned/fake/clientset_generated.go
@@ -0,0 +1,70 @@
+/*
+Copyright 2017 The etcd-operator Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package fake
+
+import (
+	clientset "github.com/coreos/etcd-operator/pkg/generated/clientset/versioned"
+	etcdv1beta2 "github.com/coreos/etcd-operator/pkg/generated/clientset/versioned/typed/etcd/v1beta2"
+	fakeetcdv1beta2 "github.com/coreos/etcd-operator/pkg/generated/clientset/versioned/typed/etcd/v1beta2/fake"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/watch"
+	"k8s.io/client-go/discovery"
+	fakediscovery "k8s.io/client-go/discovery/fake"
+	"k8s.io/client-go/testing"
+)
+
+// NewSimpleClientset returns a clientset that will respond with the provided objects.
+// It's backed by a very simple object tracker that processes creates, updates and deletions as-is,
+// without applying any validations and/or defaults. It shouldn't be considered a replacement
+// for a real clientset and is mostly useful in simple unit tests.
+func NewSimpleClientset(objects ...runtime.Object) *Clientset {
+	o := testing.NewObjectTracker(scheme, codecs.UniversalDecoder())
+	for _, obj := range objects {
+		if err := o.Add(obj); err != nil {
+			panic(err)
+		}
+	}
+
+	fakePtr := testing.Fake{}
+	fakePtr.AddReactor("*", "*", testing.ObjectReaction(o))
+
+	fakePtr.AddWatchReactor("*", testing.DefaultWatchReactor(watch.NewFake(), nil))
+
+	return &Clientset{fakePtr}
+}
+
+// Clientset implements clientset.Interface. Meant to be embedded into a
+// struct to get a default implementation. This makes faking out just the method
+// you want to test easier.
+type Clientset struct {
+	testing.Fake
+}
+
+func (c *Clientset) Discovery() discovery.DiscoveryInterface {
+	return &fakediscovery.FakeDiscovery{Fake: &c.Fake}
+}
+
+var _ clientset.Interface = &Clientset{}
+
+// EtcdV1beta2 retrieves the EtcdV1beta2Client
+func (c *Clientset) EtcdV1beta2() etcdv1beta2.EtcdV1beta2Interface {
+	return &fakeetcdv1beta2.FakeEtcdV1beta2{Fake: &c.Fake}
+}
+
+// Etcd retrieves the EtcdV1beta2Client
+func (c *Clientset) Etcd() etcdv1beta2.EtcdV1beta2Interface {
+	return &fakeetcdv1beta2.FakeEtcdV1beta2{Fake: &c.Fake}
+}
