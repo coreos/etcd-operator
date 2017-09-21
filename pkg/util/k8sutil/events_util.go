06e2ee8b5ae64b31de04b07befe3af39c984e69b
hasbro17 <hasbro17@gmail.com>
2017-09-21 12:34:32 -0700

    *: create event for new member added

---
+++ pkg/util/k8sutil/events_util.go
@@ -0,0 +1,40 @@
+package k8sutil
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	api "github.com/coreos/etcd-operator/pkg/apis/etcd/v1beta2"
+
+	"k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+func NewMemberAddEvent(memberName string, cl *api.EtcdCluster) *v1.Event {
+	t := time.Now()
+	return &v1.Event{
+		ObjectMeta: metav1.ObjectMeta{
+			GenerateName: cl.Name + "-",
+			Namespace:    cl.Namespace,
+		},
+		InvolvedObject: v1.ObjectReference{
+			APIVersion:      api.SchemeGroupVersion.String(),
+			Kind:            api.CRDResourceKind,
+			Name:            cl.Name,
+			Namespace:       cl.Namespace,
+			UID:             cl.UID,
+			ResourceVersion: cl.ResourceVersion,
+		},
+		Type:    v1.EventTypeNormal,
+		Reason:  "New Member Added",
+		Message: fmt.Sprintf("New member %s added to cluster", memberName),
+		Source: v1.EventSource{
+			Component: os.Getenv("MY_POD_NAME"),
+		},
+		// Each New Member Add event is unique so it should not be collapsed with other events
+		FirstTimestamp: metav1.Time{Time: t},
+		LastTimestamp:  metav1.Time{Time: t},
+		Count:          int32(1),
+	}
+}
