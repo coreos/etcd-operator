ed6662961b103e453a614be3cfa152ea1c3d2584
hasbro17 <hasbro17@gmail.com>
2017-10-30 13:20:40 -0700

    controller/restore-operator: use s3Reader to read backup

--- pkg/apis/etcd/v1beta2/restore_types.go
+++ pkg/apis/etcd/v1beta2/restore_types.go
@@ -48,7 +48,7 @@ type RestoreSpec struct {
 	BackupSpec BackupSpec `json:"backupSpec"`
 	// TODO: Remove BackupSpec once RestoreSource is implemented
 	// RestoreSource tells the where to get the backup and restore from.
-	RestoreSource RestoreSource `json:",inline"`
+	RestoreSource `json:",inline"`
 }
 
 type RestoreSource struct {
@@ -57,7 +57,9 @@ type RestoreSource struct {
 }
 
 type S3RestoreSource struct {
-	// The S3 path where the backup is saved.
+	// Path is the full s3 path where the backup is saved.
+	// The format of the path must be: "<s3-bucket-name>/<path-to-backup-file>"
+	// e.g: "etcd-backups/v1/default/example-etcd-cluster/3.1.8_0000000000000001_etcd.backup"
 	Path string `json:"path"`
 
 	// The name of the secret object that stores the AWS credential and config files.
