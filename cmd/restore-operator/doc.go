94c58aa200e42af4fd0994a692d4e8878c1f013a
fanmin shi <fanmin.shi@coreos.com>
2017-11-14 12:45:46 -0800

    cmd: add doc.go for restore-operator
    doc the fact that restore-operator is an example implementation and not
    recommended for production usage.

---
+++ cmd/restore-operator/doc.go
@@ -0,0 +1,19 @@
+// Copyright 2017 The etcd-operator Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// restore-operator is an example implementation of restoring etcd cluster from S3.
+// user must not depend restore-operator for production usage and should
+// follow the design principle of restore-operator to create his/her own restore
+// mechanism.
+package main
