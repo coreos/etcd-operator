a0886156b22654581dde8c93c1775040f9feeac6
fanmin shi <fanmin.shi@coreos.com>
2017-11-27 10:35:19 -0800

    backup-operator: support TLS in backup operator

--- pkg/controller/backup-operator/s3_backup.go
+++ pkg/controller/backup-operator/s3_backup.go
@@ -15,6 +15,7 @@
 package controller
 
 import (
+	"crypto/tls"
 	"fmt"
 	"path"
 
@@ -23,20 +24,34 @@ import (
 	"github.com/coreos/etcd-operator/pkg/backup/backupapi"
 	"github.com/coreos/etcd-operator/pkg/backup/writer"
 	"github.com/coreos/etcd-operator/pkg/util/awsutil/s3factory"
+	"github.com/coreos/etcd-operator/pkg/util/etcdutil"
+	"github.com/coreos/etcd-operator/pkg/util/k8sutil"
 
 	"k8s.io/client-go/kubernetes"
 )
 
 // TODO: replace this with generic backend interface for other options (PV, Azure)
 // handleS3 backups up etcd cluster to s3 and return s3 path for the backup file.
-func handleS3(kubecli kubernetes.Interface, s3 *api.S3Source, namespace, clusterName string) (string, error) {
+func handleS3(kubecli kubernetes.Interface, s3 *api.S3Source, clientTLSSecret, namespace, clusterName string) (string, error) {
 	cli, err := s3factory.NewClientFromSecret(kubecli, namespace, s3.AWSSecret)
 	if err != nil {
 		return "", err
 	}
 	defer cli.Close()
-	// TODO: support TLS.
-	bm := backup.NewBackupManagerFromWriter(kubecli, writer.NewS3Writer(cli.S3), clusterName, namespace)
+
+	var tlsConfig *tls.Config
+	if len(clientTLSSecret) != 0 {
+		d, err := k8sutil.GetTLSDataFromSecret(kubecli, namespace, clientTLSSecret)
+		if err != nil {
+			return "", fmt.Errorf("failed to get TLS data from secret (%v): %v", clientTLSSecret, err)
+		}
+		tlsConfig, err = etcdutil.NewTLSConfig(d.CertData, d.KeyData, d.CAData)
+		if err != nil {
+			return "", fmt.Errorf("failed to constructs tls config: %v", err)
+		}
+	}
+
+	bm := backup.NewBackupManagerFromWriter(kubecli, writer.NewS3Writer(cli.S3), tlsConfig, clusterName, namespace)
 	s3Prefix := backupapi.ToS3Prefix(s3.Prefix, namespace, clusterName)
 	fullPath, err := bm.SaveSnapWithPrefix(path.Join(s3.S3Bucket, s3Prefix))
 	if err != nil {
