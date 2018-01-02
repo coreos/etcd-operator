c4fab8c4e0f469046b783ffbb5c1dea63e30a8c5
hasbro17 <hasbro17@gmail.com>
2018-01-02 15:11:47 -0500

    pkg: update codegen file comments for year change

--- pkg/generated/clientset/versioned/typed/etcd/v1beta2/etcd_client.go
+++ pkg/generated/clientset/versioned/typed/etcd/v1beta2/etcd_client.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2017 The etcd-operator Authors
+Copyright 2018 The etcd-operator Authors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
