3ba08faf3814370df4c62e16b162e051b0a00876
Seth Miller <sethmiller.sm@gmail.com>
2018-08-16 17:12:42 -0500

    Update pod_util.go
    Update probe comment

--- pkg/util/k8sutil/pod_util.go
+++ pkg/util/k8sutil/pod_util.go
@@ -69,7 +69,7 @@ func containerWithRequirements(c v1.Container, r v1.ResourceRequirements) v1.Con
 }
 
 func newEtcdProbe(isSecure bool) *v1.Probe {
-	// etcd pod is alive only if a linearizable get succeeds.
+	// etcd pod is healthy only if it can participate in consensus
 	cmd := "ETCDCTL_API=3 etcdctl endpoint health"
 	if isSecure {
 		tlsFlags := fmt.Sprintf("--cert=%[1]s/%[2]s --key=%[1]s/%[3]s --cacert=%[1]s/%[4]s", operatorEtcdTLSDir, etcdutil.CliCertFile, etcdutil.CliKeyFile, etcdutil.CliCAFile)
