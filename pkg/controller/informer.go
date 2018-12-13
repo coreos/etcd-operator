b2826a0cee9b266060f02896a1d5902d693f5176
Alaykumar Patel <alay1431@gmail.com>
2018-12-13 10:36:29 -0500

    pkg/controller: Fix CR with same name in different namespaces
    This patch uses <namespace>/<cluster-name> as key to store the cluster in Controller struct to avoid over-writing same named CR's in different namespaces.
    
    Fixes #1954

--- pkg/controller/informer.go
+++ pkg/controller/informer.go
@@ -129,7 +129,7 @@ func (c *Controller) syncEtcdClus(clus *api.EtcdCluster) {
 	// re-watch or restart could give ADD event.
 	// If for an ADD event the cluster spec is invalid then it is not added to the local cache
 	// so modifying that cluster will result in another ADD event
-	if _, ok := c.clusters[clus.Name]; ok {
+	if _, ok := c.clusters[getNamespacedName(clus)]; ok {
 		ev.Type = kwatch.Modified
 	}
 
